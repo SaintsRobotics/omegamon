4a5c43df3c776353a190c07895ea3a5b1e00c910
Kevin Ji <kevin.ji@outlook.com>
2013-08-20 17:26:04 -0700

    Improve look of front page

--- front-page.php
+++ front-page.php
@@ -23,37 +23,37 @@ get_header(); ?>
 
 				<div class="front-content">
 					<div class="row">
-						<div class="eight columns panel">
-							<h2 class="panel-title">
+						<div class="eight columns infobox">
+							<h2 class="infobox-title">
 								About
 							</h2>
-							<div class="panel-content">
+							<div class="infobox-content">
 								We are <strong>Saints Robotics</strong>, team 1899, a <a href="/first/about/" title="About FIRST"><i>FIRST</i></a> Robotics Competition (FRC) team based out of Interlake High School in Bellevue, Washington. We strive to engage students in the areas of science, technology, engineering, and mathematics (STEM) by allowing them to work on the construction of physical robots.
 							</div>
 						</div>
 					</div>
 					<div class="row">
-						<div class="four columns panel">
-							<h2 class="panel-title">
+						<div class="four columns infobox">
+							<h2 class="infobox-title">
 								<i>FIRST</i>
 							</h2>
-							<div class="panel-content">
+							<div class="infobox-content">
 								<a href="/first/about/" title="About FIRST"><i>FIRST</i></a> (For Inspiration and Recognition of Science and Technology) is a non-profit organization based in Manchester, New Hampshire. Founded in 1989 by Inventor Dean Kamen and Professor Woodie Flowers, <i>FIRST</i> aims "to create a world where science and technology are celebrated&hellip; where young people dream of becoming science and technology heroes."
 							</div>
 						</div>
-						<div class="four columns panel">
-							<h2 class="panel-title">
+						<div class="four columns infobox">
+							<h2 class="infobox-title">
 								Current FRC Competition: Ultimate Ascent
 							</h2>
-							<div class="panel-content">
+							<div class="infobox-content">
 								This year's competition, <a href="/first/2013-competition/" title="2013 Competition: Ultimate Ascent">Ultimate Ascent</a>, is a combination of ultimate frisbee and ascending a pyramid. Two teams of three alliances each aim to outscore the other by earning as many points as possible in the autonomous and teleoperated periods of a match.
 							</div>
 						</div>
-						<div class="four columns panel">
-							<h2 class="panel-title">
+						<div class="four columns infobox">
+							<h2 class="infobox-title">
 								Helping Hounds
 							</h2>
-							<div class="panel-content">
+							<div class="infobox-content">
 								Our <a href="/community/" title="Helping Hounds Program">Helping Hounds Program</a> is our club's community service program. Through various fundraisers, science fairs, and other outreach activities, we have actively promoted STEM in our community and beyond.
 							</div>
 						</div>
