e80c8683ecc2d3459355d50aba0cf3e8dba967ca
Kevin Ji <kevin.ji@outlook.com>
2013-08-20 19:05:36 -0700

    Use hyphens

--- header.php
+++ header.php
@@ -31,11 +31,11 @@
 	// Add the blog description for the home/front page.
 	$site_description = get_bloginfo( 'description', 'display' );
 	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+		echo " - $site_description";
 
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'toolbox' ), max( $paged, $page ) );
+		echo ' - ' . sprintf( __( 'Page %s', 'toolbox' ), max( $paged, $page ) );
 
 	?></title>
 
