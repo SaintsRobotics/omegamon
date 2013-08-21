7a0f8d2389d5e21779e4ee79019f8934dea08688
Kevin Ji <kevin.ji@outlook.com>
2013-08-20 17:16:05 -0700

    Expand on front page

--- front-page.php
+++ front-page.php
@@ -23,7 +23,7 @@ get_header(); ?>
 
 				<div class="front-content">
 					<div class="row">
-						<div class="twelve columns panel">
+						<div class="eight columns panel">
 							<h2 class="panel-title">
 								About
 							</h2>
@@ -32,8 +32,8 @@ get_header(); ?>
 							</div>
 						</div>
 					</div>
-					<?php /*<div class="row">
-						<div class="three columns panel">
+					<div class="row">
+						<div class="four columns panel">
 							<h2 class="panel-title">
 								<i>FIRST</i>
 							</h2>
@@ -41,7 +41,23 @@ get_header(); ?>
 								<a href="/first/about/" title="About FIRST"><i>FIRST</i></a> (For Inspiration and Recognition of Science and Technology) is a non-profit organization based in Manchester, New Hampshire. Founded in 1989 by Inventor Dean Kamen and Professor Woodie Flowers, <i>FIRST</i> aims "to create a world where science and technology are celebrated&hellip; where young people dream of becoming science and technology heroes."
 							</div>
 						</div>
-					</div>*/ ?>
+						<div class="four columns panel">
+							<h2 class="panel-title">
+								Current FRC Competition: Ultimate Ascent
+							</h2>
+							<div class="panel-content">
+								This year's competition, <a href="/first/2013-competition/" title="2013 Competition: Ultimate Ascent">Ultimate Ascent</a>, is a combination of ultimate frisbee and ascending a pyramid. Two teams of three alliances each aim to outscore the other by earning as many points as possible in the autonomous and teleoperated periods of a match.
+							</div>
+						</div>
+						<div class="four columns panel">
+							<h2 class="panel-title">
+								Helping Hounds
+							</h2>
+							<div class="panel-content">
+								Our <a href="/community/" title="Helping Hounds Program">Helping Hounds Program</a> is our club's community service program. Through various fundraisers, science fairs, and other outreach activities, we have actively promoted STEM in our community and beyond.
+							</div>
+						</div>
+					</div>
 				</div>
 
 			</div><!-- #content -->
