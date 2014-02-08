759b3a24be5a85a95670584c8683915927428d0f
Kevin Ji <kevin.ji@outlook.com>
2014-02-08 10:12:04 -0800

    Use Universal Analytics
    Fixes #4

--- js/app.js
+++ js/app.js
@@ -67,16 +67,13 @@
 				easing: "swing"
 			});
 		});
+
+	// Google Analytics
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-20132041-1', 'saintsrobotics.com');
+	ga('send', 'pageview');
 })(jQuery, this);
-
-// Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-20132041-1']);
-_gaq.push(['_setDomainName', 'saintsrobotics.com']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
